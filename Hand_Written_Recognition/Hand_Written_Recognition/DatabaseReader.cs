3e98498b1e07f09c1b52c34ce0f60c3e5f78bf74
Hantao Zhong <chineseiven@users.noreply.github.com>
2018-03-19 16:49:19 +0000

    Start Project
    Initial Files and Datareader class

---
+++ Hand_Written_Recognition/Hand_Written_Recognition/DatabaseReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hand_Written_Recognition
+{
+    class DatabaseReader
+    {
+        public void Database()
+        {
+            StreamReader reader = new StreamReader("train-images.idx3-ubyte");
+            string line = reader.ReadToEnd();
+            MessageBox.Show(line);
+        }
+    }
+}
