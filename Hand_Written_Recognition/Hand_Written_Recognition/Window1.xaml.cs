7123c20fb9517fafa315d18c8af54e74c254463e
Hantao Zhong <chineseiven@users.noreply.github.com>
2018-05-17 23:09:13 +0100

    Number display function
    Use canvas to display the number from the MINST database

--- Hand_Written_Recognition/Hand_Written_Recognition/Window1.xaml.cs
+++ Hand_Written_Recognition/Hand_Written_Recognition/Window1.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Drawing;
 
 namespace Hand_Written_Recognition
 {
@@ -24,11 +26,63 @@ namespace Hand_Written_Recognition
             InitializeComponent();
         }
 
+
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
             DatabaseReader databaseReader = new DatabaseReader();
+            Drawing drawing = new Drawing();
             //MessageBox.Show(databaseReader.Database());
-            imagebox.Source= databaseReader.Image();
+
+            byte[] vs = databaseReader.pixInfrom();
+
+            Drawit(vs);
+
+            
+           
+
+        }
+
+        
+
+        private void Drawit(byte[] vs)
+        {
+            //int mag = 1;
+            //byte pixInfo= 0;
+            //var brush = new SolidColorBrush(Color.FromRgb(pixInfo, pixInfo, pixInfo));
+            Can.Margin = new Thickness(0, 0, 0, 0);
+            //Rectangle r = new Rectangle();
+            for (int count=0; count< vs.Length; count++)
+            {
+
+                Rectangle r = new Rectangle();
+                r.Fill = new SolidColorBrush(Color.FromRgb(vs[count], vs[count], vs[count]));
+                r.Stroke = new SolidColorBrush(Colors.Red);
+                r.Width = 8;
+                r.Height = 8;
+                double[] db = CoodCheck(count);
+                double x = db[0];
+                double y = db[1];
+                r.SetValue(Canvas.LeftProperty, x);
+                r.SetValue(Canvas.TopProperty, y);
+                Can.Children.Add(r);
+                //MessageBox.Show("run" + count);
+            }
+   
+        }
+
+        public double[] CoodCheck(int NumberOfPix)
+        {
+            double x = 50;
+            double y = 50;
+
+            x = x + ((NumberOfPix % 28) * (8));
+            y = y + ((NumberOfPix / 28)*  (8));
+            //MessageBox.Show("x=" + x + ",y=" + y);
+            double[] re = new double[2];
+            re[0] = x;
+            re[1] = y;
+            
+            return re;
         }
     }
 }
